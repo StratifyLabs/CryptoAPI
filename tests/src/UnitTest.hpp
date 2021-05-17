d393917da23ca393823b0471ada6f0a2392c0de2
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-05-17 11:15:30 -0600

    Update UnitTest.hpp

--- tests/src/UnitTest.hpp
+++ tests/src/UnitTest.hpp
@@ -42,23 +42,20 @@ public:
 
     Random().seed().randomize(View(buffer));
 
-    printer().key("buffer", View(buffer).to_string());
+    printer().key("buffer", View(buffer).to_string<GeneralString>());
 
     printer().key(
-      "random",
-      DataFile()
-        .reserve(12)
-        .write(NullFile(12), Random().seed())
-        .data()
-        .to_string());
+        "random",
+        View(DataFile().reserve(12).write(NullFile(12), Random().seed()).data())
+            .to_string<GeneralString>());
 
     return true;
   }
 
   bool sha256_api_case() {
 
-    const StringView input
-      = "With the newest hardware (CPU and GPU) improvements it is become "
+    const StringView input =
+        "With the newest hardware (CPU and GPU) improvements it is become "
         "possible to decrypt SHA256 algorithm back. So it is no longer "
         "recommended to use it for password protection or other similar use "
         "cases. Some years ago you would protect your passwords from hackers "
@@ -66,7 +63,7 @@ public:
         "no longer a case.";
 
     const Data output = Data::from_string(
-      "81bb7b29be293aeab191404e671a8a8c7687756b632cbd6a29ee65f909b30bcf");
+        "81bb7b29be293aeab191404e671a8a8c7687756b632cbd6a29ee65f909b30bcf");
 
     {
       Sha256 sha256;
@@ -83,95 +80,87 @@ public:
     Printer::Object po(printer(), "aes_api_case()");
     const Data key128 = Data::from_string("E8E9EAEBEDEEEFF0F2F3F4F5F7F8F9FA");
     const Data key256 = Data::from_string(
-      "08090A0B0D0E0F10121314151718191A1C1D1E1F21222324262728292B2C2D2E");
+        "08090A0B0D0E0F10121314151718191A1C1D1E1F21222324262728292B2C2D2E");
     const Data plain128 = Data::from_string("014BAF2278A69D331D5180103643E99A");
     const Data plain256 = Data::from_string("069A007FC76A459F98BAF917FEDF9521");
     {
       Printer::Object po(printer(), "ecb");
 
-      const Data cipher128
-        = Data::from_string("6743C3D1519AB4F2CD9A78AB09A511BD");
+      const Data cipher128 =
+          Data::from_string("6743C3D1519AB4F2CD9A78AB09A511BD");
 
       PRINTER_TRACE(printer(), "");
 
-      const Data cipher256
-        = Data::from_string("080e9517eb1677719acf728086040ae3");
+      const Data cipher256 =
+          Data::from_string("080e9517eb1677719acf728086040ae3");
 
       PRINTER_TRACE(printer(), "");
-      TEST_ASSERT(
-        Aes().set_key128(key128).encrypt_ecb(View(plain128)) == cipher128);
+      TEST_ASSERT(Aes().set_key128(key128).encrypt_ecb(View(plain128)) ==
+                  cipher128);
 
       PRINTER_TRACE(printer(), "");
-      TEST_ASSERT(
-        Aes().set_key256(key256).encrypt_ecb(View(plain256)) == cipher256);
+      TEST_ASSERT(Aes().set_key256(key256).encrypt_ecb(View(plain256)) ==
+                  cipher256);
 
       PRINTER_TRACE(printer(), "");
-      TEST_ASSERT(
-        Aes().set_key128(key128).decrypt_ecb(View(cipher128)) == plain128);
+      TEST_ASSERT(Aes().set_key128(key128).decrypt_ecb(View(cipher128)) ==
+                  plain128);
 
       PRINTER_TRACE(printer(), "");
-      TEST_ASSERT(
-        Aes().set_key256(key256).decrypt_ecb(View(cipher256)) == plain256);
+      TEST_ASSERT(Aes().set_key256(key256).decrypt_ecb(View(cipher256)) ==
+                  plain256);
     }
 
     {
       Printer::Object po(printer(), "cbc");
       const Data iv = Data::from_string("E8E9EAEBEDEEEFF0F2F3F4F5F7F8F9FA");
 
-      const Data cipher128
-        = Data::from_string("d538ce674bcbf555aba94b74bba25e45");
+      const Data cipher128 =
+          Data::from_string("d538ce674bcbf555aba94b74bba25e45");
 
       PRINTER_TRACE(printer(), "");
 
-      const Data cipher256
-        = Data::from_string("c56b404deb3764bd3805eb3f10204e5c");
-
-      PRINTER_TRACE(printer(), "");
-      TEST_ASSERT(
-        Aes().set_initialization_vector(iv).set_key128(key128).encrypt_cbc(
-          View(plain128))
-        == cipher128);
+      const Data cipher256 =
+          Data::from_string("c56b404deb3764bd3805eb3f10204e5c");
 
       PRINTER_TRACE(printer(), "");
       TEST_ASSERT(
-        Aes().set_initialization_vector(iv).set_key256(key256).encrypt_cbc(
-          View(plain256))
-        == cipher256);
+          Aes().set_initialization_vector(iv).set_key128(key128).encrypt_cbc(
+              View(plain128)) == cipher128);
 
       PRINTER_TRACE(printer(), "");
       TEST_ASSERT(
-        Aes().set_initialization_vector(iv).set_key128(key128).decrypt_cbc(
-          View(cipher128))
-        == plain128);
+          Aes().set_initialization_vector(iv).set_key256(key256).encrypt_cbc(
+              View(plain256)) == cipher256);
 
       PRINTER_TRACE(printer(), "");
       TEST_ASSERT(
-        Aes().set_initialization_vector(iv).set_key256(key256).decrypt_cbc(
-          View(cipher256))
-        == plain256);
+          Aes().set_initialization_vector(iv).set_key128(key128).decrypt_cbc(
+              View(cipher128)) == plain128);
+
+      PRINTER_TRACE(printer(), "");
+      TEST_ASSERT(
+          Aes().set_initialization_vector(iv).set_key256(key256).decrypt_cbc(
+              View(cipher256)) == plain256);
 
       {
         Printer::Object po(printer(), "cbc-file");
 
-        TEST_ASSERT(
-          View(DataFile()
-                 .reserve(16)
-                 .write(
-                   ViewFile(View(plain128)),
-                   AesCbcEncrypter().set_initialization_vector(iv).set_key128(
-                     key128))
-                 .data())
-          == View(cipher128));
+        TEST_ASSERT(View(DataFile()
+                             .reserve(16)
+                             .write(ViewFile(View(plain128)),
+                                    AesCbcEncrypter()
+                                        .set_initialization_vector(iv)
+                                        .set_key128(key128))
+                             .data()) == View(cipher128));
 
-        TEST_ASSERT(
-          View(DataFile()
-                 .reserve(32)
-                 .write(
-                   ViewFile(View(plain256)),
-                   AesCbcEncrypter().set_initialization_vector(iv).set_key256(
-                     key256))
-                 .data())
-          == View(cipher256));
+        TEST_ASSERT(View(DataFile()
+                             .reserve(32)
+                             .write(ViewFile(View(plain256)),
+                                    AesCbcEncrypter()
+                                        .set_initialization_vector(iv)
+                                        .set_key256(key256))
+                             .data()) == View(cipher256));
       }
     }
 
