21e660f79530d2f63d40378bda541bd89aac2400
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-11-04 21:16:30 -0500

    Remove extra includes from test. cherry-pick: main

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,19 +5,14 @@
 #include "UnitTest.hpp"
 
 #define VERSION "0.1"
-#include "sys/Cli.hpp"
+#include <sys/Cli.hpp>
 
 using namespace test;
-
-void segfault(int a) { API_ASSERT(false); }
 
 int main(int argc, char *argv[]) {
   sys::Cli cli(argc, argv);
 
-#if defined __link
-  signal(11, segfault);
-#endif
-
+  api::catch_segmentation_fault();
 
   {
     auto scope = Test::Scope<printer::Printer>(test::Test::Initialize()
